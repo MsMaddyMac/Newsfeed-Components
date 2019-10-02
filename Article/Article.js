6c93f6e97157272b8bafbe8d8ce133360bce84ab
Madeline <mmcintosh2k14@gmail.com>
2019-10-02 16:35:37 -0400

    adds completed project

--- Article/Article.js
+++ Article/Article.js
@@ -167,20 +167,23 @@ const data = [
     articleContent2.textContent = secondParagraph;
     articleContent3.textContent = thirdParagraph;
 
+/* Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div. */
+
+    article.addEventListener('click', (e) => {
+      console.log('button clicked', e.target);
+      article.classList.toggle('article-open');
+    })
+
+/* Step 3: return the entire component. */
+
     return article
   }
+
+  /* Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div. */
 
   data.forEach(info => {
     articles.appendChild(createArticle(info.title, info.date, info.firstParagraph, info.secondParagraph, info.thirdParagraph))
   })
-
-/* Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div. */
-
-
-
-/* Step 3: return the entire component. */
-
-/* Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div. */
 
 /* Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article. */
 
